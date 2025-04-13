fa5b0e8cc9fe1b8f60c4d0cec1ba96c5b2736680
Jackson Conrad <jacksonator0@gmail.com>
2025-04-13 08:28:57 -0500

    beginning of decision_tree

---
+++ data_structures/decision_tree.py
@@ -0,0 +1,17 @@
+class Node:
+    def __init__(self, left=None, right=None):
+        self.question = get_best_question()
+        self.result
+
+def add_data(data: dict):
+    #TODO error handling for mismatched dict types
+    data.append(data)
+
+def get_best_question():
+    #TODO find question that splits possible answers as evenly as possible
+    return #the best question
+
+def check_gini_impurity():
+    #TODO checks if data is sufficent for proper analysis
+
+data = []
